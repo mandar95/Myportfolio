cebc76b6f3fe46b24dd2cc059faf21d82cb8d743
mandar95 <mandarjaurat@gmail.com>
2020-07-08 17:01:43 +0530

    Updates

---
+++ precache-manifest.b48c6d021e902f574ee26678f3537222.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eeb3959d54d4e21d9261bf20058436a9",
+    "url": "/MJrepository95/index.html"
+  },
+  {
+    "revision": "b4814dd19601245b0cd0",
+    "url": "/MJrepository95/static/css/2.6a21e6c8.chunk.css"
+  },
+  {
+    "revision": "6aa7b0be39bfb587b1b9",
+    "url": "/MJrepository95/static/css/main.2ae85ec8.chunk.css"
+  },
+  {
+    "revision": "b4814dd19601245b0cd0",
+    "url": "/MJrepository95/static/js/2.36989db4.chunk.js"
+  },
+  {
+    "revision": "493402937001f837902b42f20b84dbeb",
+    "url": "/MJrepository95/static/js/2.36989db4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6aa7b0be39bfb587b1b9",
+    "url": "/MJrepository95/static/js/main.b615997b.chunk.js"
+  },
+  {
+    "revision": "2733fc4e785dbbc2e319",
+    "url": "/MJrepository95/static/js/runtime-main.414f86af.js"
+  },
+  {
+    "revision": "6d4173fba73d7507a3e0863ce595e33a",
+    "url": "/MJrepository95/static/media/1.6d4173fb.jpeg"
+  },
+  {
+    "revision": "29d328d45cad9d454e8b5976e1ef3571",
+    "url": "/MJrepository95/static/media/2.29d328d4.jpeg"
+  },
+  {
+    "revision": "06ab33ae3e2cbae0f509cd05f2ee6793",
+    "url": "/MJrepository95/static/media/boxicons.06ab33ae.eot"
+  },
+  {
+    "revision": "5bbebd73a37de0cc242e872b4483269e",
+    "url": "/MJrepository95/static/media/boxicons.5bbebd73.woff"
+  },
+  {
+    "revision": "5e99edcb80abc73c7a06aacfddca51bf",
+    "url": "/MJrepository95/static/media/boxicons.5e99edcb.svg"
+  },
+  {
+    "revision": "72589e162de78952d4d6b5ce8b832175",
+    "url": "/MJrepository95/static/media/boxicons.72589e16.ttf"
+  },
+  {
+    "revision": "aeaf4ee6e2c712532f982c8a4dd68b16",
+    "url": "/MJrepository95/static/media/boxicons.aeaf4ee6.woff2"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8b0385a7a8fa9c0f83666bb46ea0112c.js"
+  "/MJrepository95/precache-manifest.b48c6d021e902f574ee26678f3537222.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/MJrepository95/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
